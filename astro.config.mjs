505adc5af60083dfc4bf61b1f6c283911b4ca858
Saruuuu19 <aretamoso99@gmail.com>
2025-11-10 12:43:01 -0500

    fixed trailingslash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  
-  trailingSlash: 'never',
+
+  trailingSlash: 'always',
 
   vite: {
     plugins: [tailwindcss()]
